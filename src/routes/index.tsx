afb528b9d5fee431bddcc00987872f619214892f
ruhancmendes <ruhancmendes@gmail.com>
2025-11-27 17:43:05 -0300

    feat: add Employee routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,12 @@
 import { BrowserRouter } from "react-router";
 
 import { AuthRoutes } from "./AuthRoutes";
+import { EmployeeRoutes } from "./EmployeeRoutes"
 
 export function Routes() {
   return (
     <BrowserRouter>
-      <AuthRoutes />
+      <EmployeeRoutes />
     </BrowserRouter>
   );
 }
