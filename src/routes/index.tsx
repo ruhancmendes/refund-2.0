bb6b55f90dc03f0c30d67cabe5f254426d148cf3
ruhancmendes <ruhancmendes@gmail.com>
2025-11-28 11:00:27 -0300

    feat: add Manager routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,41 @@
 import { BrowserRouter } from "react-router";
 
 import { AuthRoutes } from "./AuthRoutes";
-import { EmployeeRoutes } from "./EmployeeRoutes"
+import { ManagerRoutes } from "./ManagerRoutes";
+import { EmployeeRoutes } from "./EmployeeRoutes";
+
+import { Loading } from "../components/Loading";
+
+const isLoading = false;
+//const session = undefined
+
+const session = {
+  user: {
+    role: "",
+  },
+};
 
 export function Routes() {
+  function Route() {
+    switch (session?.user.role) {
+      case "employee":
+        return <EmployeeRoutes />;
+
+      case "manager":
+        return <ManagerRoutes />;
+
+      default:
+        return <AuthRoutes />;
+    }
+  }
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <BrowserRouter>
-      <EmployeeRoutes />
+      <Route />
     </BrowserRouter>
   );
 }
